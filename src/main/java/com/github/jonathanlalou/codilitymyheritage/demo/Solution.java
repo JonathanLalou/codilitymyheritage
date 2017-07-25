f54d2574e39076892acb0a8f67365d7d00f50bc5
Jonathan LALOU <Jonathan.Lalou@gmail.com>
2017-07-24 23:06:36 -0400

    solved demo

--- src/main/java/com/github/jonathanlalou/codilitymyheritage/demo/Solution.java
+++ src/main/java/com/github/jonathanlalou/codilitymyheritage/demo/Solution.java
@@ -5,8 +5,29 @@ package com.github.jonathanlalou.codilitymyheritage.demo;// you can also use imp
 // System.out.println("this is a debug message");
 
 class Solution {
-    public int solution(int[] A) {
+    public int solution(int[] input) {
         // write your code in Java SE 8
         Integer index;
+        for (int i = 0; i < input.length; i++) {
+            int current = input[i];
+            final int sumBefore = sum(input, 0, i);
+            final int sumAfter = sum(input, i + 1, input.length);
+            System.out.println("\n index: " + i + "  ");
+            System.out.printf(" sumBefore %d", sumBefore);
+            System.out.printf(" sumAfter %d", sumAfter);
+            if (sumBefore == sumAfter) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int sum(int[] input, int from, int to) {
+        Integer sum = 0;
+        for (int i = from; i < to; i++) {
+            int current = input[i];
+            sum += current;
+        }
+        return sum;
     }
 }
--- src/main/test/unit/com/github/jonathanlalou/codilitymyheritage/demo/SolutionUnitTest.java
+++ src/main/test/unit/com/github/jonathanlalou/codilitymyheritage/demo/SolutionUnitTest.java
@@ -6,19 +6,19 @@ import static org.junit.Assert.assertEquals;
 
 public class SolutionUnitTest {
     final Solution solution = new Solution();
+    private int[] input = new int[]{
+            -1,
+            3,
+            -4,
+            5,
+            1,
+            -6,
+            2,
+            1,
+    };
 
     @Test
     public void testSolution() {
-        final int[] input = new int[]{
-                -1,
-                3,
-                4,
-                5,
-                1,
-                6,
-                2,
-                1,
-        };
         assertEquals(1, solution.solution(input));
     }
 
