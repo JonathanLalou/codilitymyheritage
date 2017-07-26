f04f34e9b1179deb0f057291085851a1b8dd9440
Jonathan LALOU <Jonathan.Lalou@gmail.com>
2017-07-25 23:18:33 -0400

    fixed task 2!

--- src/main/java/com/github/jonathanlalou/codilitymyheritage/exo2/Solution.java
+++ src/main/java/com/github/jonathanlalou/codilitymyheritage/exo2/Solution.java
@@ -2,15 +2,24 @@ package com.github.jonathanlalou.codilitymyheritage.exo2;
 
 public class Solution {
     public int solution(int[] A) {
-        // write your code in Java SE 8
-        return -1;
+        final int[] diffs = new int[A.length];
+
+        for (int j = 0; j < A.length; j++) {
+            final int maxLeft = max(A, 0, j);
+            final int maxRight = max(A, j, A.length);
+//            System.out.printf("\n j :%d, maxLeft: %d, maxRight: %d", j, maxLeft, maxRight);
+            diffs[j] = Math.abs(maxLeft - maxRight);
+        }
+
+
+        return max(diffs, 0, A.length);
     }
 
     protected int max(int[] A, int from, int to) {
         int max = A[from];
-        for (int i = from; i < to; i++){
-            System.out.println("i, A[i] == " + i + " " + A[i]);
-            if (A[i]>max){
+        for (int i = from; i < to; i++) {
+//            System.out.println("i, A[i] == " + i + " " + A[i]);
+            if (A[i] > max) {
                 max = A[i];
             }
         }
--- src/main/test/unit/com/github/jonathanlalou/codilitymyheritage/exo2/Solution2UnitTest.java
+++ src/main/test/unit/com/github/jonathanlalou/codilitymyheritage/exo2/Solution2UnitTest.java
@@ -4,16 +4,17 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class Solution2Test {
+public class Solution2UnitTest {
     private Solution solution = new Solution();
 
     @Test
-    public void solution2(){
-
+    public void solution2() {
+        assertEquals(6, solution.solution(new int[]{1, 3, -3}));
+        assertEquals(4, solution.solution(new int[]{4, 3, 2, 5, 1, 1}));
     }
 
     @Test
-    public void max(){
+    public void max() {
         final int[] A = {1, 15, 65, 12, 5, 6, 8, 7, 9, 77, 5, 99, 5, 10, 5, 66};
         assertEquals(65, solution.max(A, 0, 6));
         assertEquals(99, solution.max(A, 3, 13));
