dc9dd54dceac4517ac42353d0cb86ef8a54e4f0b
Jonathan LALOU <Jonathan.Lalou@gmail.com>
2017-07-25 23:38:28 -0400

    solved task 1!!

--- src/main/java/com/github/jonathanlalou/codilitymyheritage/exo1/Solution.java
+++ src/main/java/com/github/jonathanlalou/codilitymyheritage/exo1/Solution.java
@@ -1,4 +1,32 @@
 package com.github.jonathanlalou.codilitymyheritage.exo1;
 
 public class Solution {
+    public int solution(int[] A) {
+        int max = -1;
+        for (int p = 0; p < A.length - 2; p++) {
+            for (int q = p + 1; q < A.length - 1; q++) {
+                for (int r = q + 1; r < A.length; r++) {
+                    if (isTriangle(A, p, q, r)) {
+                        int perimeter = perimeter(A, p, q, r);
+//                        System.out.println("p, q, r, perimeter: " + p + " " + q + " " + r + " " + perimeter);
+                        if (perimeter > max) {
+                            max = perimeter;
+                        }
+                    }
+                }
+            }
+        }
+        return max;
+    }
+
+    public boolean isTriangle(int[] A, int p, int q, int r) {
+        return (0 <= p) && (p < q) && (q < r) && (r < A.length)
+                && ((A[p] + A[q]) > A[r])
+                && ((A[q] + A[r]) > A[p])
+                && ((A[r] + A[p]) > A[q]);
+    }
+
+    public int perimeter(int[] A, int p, int q, int r) {
+        return A[p] + A[q] + A[r];
+    }
 }
