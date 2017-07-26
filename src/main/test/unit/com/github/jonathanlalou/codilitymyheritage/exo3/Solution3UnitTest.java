fca34373259e3ee4311c6a1b2fc55c9093ccf061
Jonathan LALOU <Jonathan.Lalou@gmail.com>
2017-07-25 22:51:38 -0400

    have found a unit test that fails!!!

--- src/main/test/unit/com/github/jonathanlalou/codilitymyheritage/exo3/Solution3UnitTest.java
+++ src/main/test/unit/com/github/jonathanlalou/codilitymyheritage/exo3/Solution3UnitTest.java
@@ -21,4 +21,46 @@ public class Solution3UnitTest {
         assertEquals(-1, solution.solution(A, B));
     }
 
+    @Test
+    public void solution_3() throws Exception {
+        int[] A = new int[]{1, 2, 5, 6, 7, 19};
+        int[] B = new int[]{19, 32, 45};
+        assertEquals(19, solution.solution(A, B));
+    }
+
+    @Test
+    public void solution_4() throws Exception {
+        int[] A = new int[]{1, 2, 5, 6, 7, 19};
+        int[] B = new int[]{1, 2};
+        assertEquals(1, solution.solution(A, B));
+    }
+
+    @Test
+    public void solution_4_2() throws Exception {
+        int[] A = new int[]{1, 2, 5, 6, 7, 19};
+        int[] B = new int[]{1, 2, 5, 6, 7, 19};
+        assertEquals(1, solution.solution(A, B));
+    }
+
+    @Test
+    public void solution_4_3() throws Exception {
+        int[] A = new int[]{2, 2, 5, 6, 7, 19};
+        int[] B = new int[]{1, 1, 1, 2, 7, 19};
+        assertEquals(2, solution.solution(A, B));
+    }
+
+    @Test
+    public void solution_5() throws Exception {
+        int[] A = new int[]{1};
+        int[] B = new int[]{32, 18, 1, 2};
+        assertEquals(1, solution.solution(A, B));
+    }
+
+    @Test
+    public void solution_6() throws Exception {
+        int[] A = new int[]{1, 18};
+        int[] B = new int[]{1, 18};
+        assertEquals(1, solution.solution(A, B));
+    }
+
 }
